668bfceb99ed4a292bf5b7a883a2e113923465b9
metal-crow <frankcrisanti@aol.com>
2016-03-11 21:57:35 -0500

    testing basic api

--- README.md
+++ README.md
@@ -4,3 +4,9 @@ install Rust x64
 install MSYS2
 -mingw-w64-x86_64-gcc, add C:\MSYS64\mingw64\bin to PATH
 -install mingw-w64-x86_64-openssl
+
+behind webauth
+
+
+store the name of the item
+quantity
--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,74 @@
 extern crate iron;
 extern crate router;
 extern crate rustc_serialize;
+extern crate mysql;
 
 use iron::prelude::*;
 use iron::status;
 use router::Router;
 use std::io::Read;
 use rustc_serialize::json;
+use mysql as my;
 
-#[derive(RustcEncodable)]
-struct Greeting {
-    msg: String
+//struct in database
+#[derive(Debug, PartialEq, Eq)]
+#[derive(RustcEncodable, RustcDecodable)]
+struct Item {
+    item_name: String,
+    quantity: i32,
+    description: Option<String>,
 }
 
-fn hello_world(_: &mut Request) -> IronResult<Response> {
-	println!("GET");
-    let greeting = Greeting { msg: "Hello World".to_string() };
-    let payload = json::encode(&greeting).unwrap();
+//json request format for get_item
+#[derive(RustcEncodable, RustcDecodable)]
+struct GetItemRequest {
+    item_name: String,
+}
+fn get_item_info(request: &mut Request, pool: &my::Pool) -> IronResult<Response> {
+	//get request json and convert to struct
+	let mut payload = String::new();
+    request.body.read_to_string(&mut payload).unwrap();
+    let request: GetItemRequest = json::decode(&payload).unwrap();
+    
+    let mut statement = pool.prepare("SELECT item_name,quantity,description from test.inventory WHERE item_name=?").unwrap();
+
+    //execute the sql query with the data from the struct
+    let selected_items: Vec<Item> = statement.execute((request.item_name,))    
+    .map(|result| {
+    	// In this closure we sill map `QueryResult` to `Vec<Payment>`
+        // `QueryResult` is iterator over `MyResult<row, err>` so first call to `map`
+        // will map each `MyResult` to contained `row` (no proper error handling)
+        // and second call to `map` will map each `row` to `Payment`
+        result.map(|x| x.unwrap()).map(|row| {
+            let (item_name, quantity, description) = my::from_row(row);
+            Item {
+                item_name: item_name,
+                quantity: quantity,
+                description: description,
+            }
+        }).collect() // Collect payments so now `QueryResult` is mapped to `Vec<Payment>`
+    }).unwrap(); // Unwrap `Vec<Payment>`
+    
+    //convert the sql result to json and return
+    let payload = json::encode(&selected_items).unwrap();
     Ok(Response::with((status::Ok, payload)))
 }
 
 // Receive a message by POST and play it back.
-fn set_greeting(request: &mut Request) -> IronResult<Response> {
-    let mut payload = String::new();
-    request.body.read_to_string(&mut payload).unwrap();
-    let request: Greeting = json::decode(&payload).unwrap();
-    //let greeting = Greeting { msg: request.msg };
-    //let payload = json::encode(&greeting).unwrap();
-    Ok(Response::with((status::Ok)))
+fn add_item_to_inventory(request: &mut Request, pool: &my::Pool) -> IronResult<Response> {
+    Ok(Response::with(status::Ok))
 }
 
 fn main() {
+	let pool = my::Pool::new("mysql://root:test@localhost:3306").unwrap();
+	let pool_clone = pool.clone();
+
 	let mut router = Router::new();
-    router.get("/", hello_world);
-    router.post("/set", set_greeting);
+	
+    router.post("/ItemInfo", move |r: &mut Request| get_item_info(r, &pool));
     
-    Iron::new(hello_world).http("localhost:3000").unwrap();
+    router.post("/ItemAdd" , move |r: &mut Request| add_item_to_inventory(r, &pool_clone));
+        
+    Iron::new(router).http("localhost:3000").unwrap();
     println!("On 3000");
 }
