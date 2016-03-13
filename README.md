281c73079d3815c13e84d547d20261730a202d82
metal-crow <frankcrisanti@aol.com>
2016-03-12 20:52:40 -0500

    using objects for single db access location

--- README.md
+++ README.md
@@ -10,5 +10,13 @@ Windows Setup:
   * install mingw-w64-x86_64-openssl
  * MySQL
  
+###Features
+Server API:
+ * Request info about item
+ * Add item to inventory
+ * **Search for item based on name or description**
+Client:
+ * Get and display info about item 
+ 
 ###ToDo 
 behind webauth  
--- src/server.rs
+++ src/server.rs
@@ -8,7 +8,6 @@ use iron::status;
 use router::Router;
 use std::io::Read;
 use rustc_serialize::json;
-use mysql as my;
 use std::error::Error;
 
 //struct in database
@@ -19,43 +18,90 @@ struct Item {
     quantity: i32,
     description: Option<String>,
 }
+static MYSQL_DB_COLS: &'static str = "item_name,quantity,description";
+
+struct DatabaseManager{
+	pool : mysql::Pool,
+}
+
+impl DatabaseManager {
+	//handles querying the database, and returing an array of the results in Item form. Only function that has access to Pool
+	fn results_from_database(&self, query : String) -> Result<Vec<Item>, mysql::Error> {
+		let mut statement = match self.pool.prepare(query) {
+			Ok(s) => s,
+			Err(err) => return Err(err),
+		};
+		
+		let selected_items: Result<Vec<Item>, _> = statement.execute(()).map(|result| {
+	    	// In this closure we sill map `QueryResult` to `Vec`
+	        // `QueryResult` is iterator over `MyResult<row, err>` so first call to `map`
+	        // will map each `MyResult` to contained `row` (no proper error handling)
+	        // and second call to `map` will map each `row` to `Payment`
+	        result.map(|x| x.unwrap()).map(|row| {
+	            let (item_name, quantity, description) = mysql::from_row(row);
+	            Item {
+	                item_name: item_name,
+	                quantity: quantity,
+	                description: description,
+	            }
+	        }).collect() // Collect payments so now `QueryResult` is mapped to `Vec`
+	    });
+	    
+	    return selected_items
+	}
+	
+	//handle inserting into the database or any other action that doesnt return a result, but can error
+	fn insert_into_database(&self, items: Vec<Item>) -> Vec<String> {
+		//allow execute of valid comands, but report errors
+		let mut errors: Vec<String> = Vec::new();
+
+		//insert into database
+		for mut stmt in self.pool.prepare(format!("INSERT into test.inventory ({0}) VALUES (?,?,?)",MYSQL_DB_COLS)).into_iter() {
+	        for i in items.iter() {
+	            match stmt.execute((&i.item_name, i.quantity, &i.description)) {
+	            	Ok(_) => continue,
+	            	Err(err) => errors.push(err.to_string()),
+	            }
+	        }
+	    }
+		
+		return errors;
+	}
+
+}
+
+
+
 
 //json request format for get_item
 #[derive(RustcEncodable, RustcDecodable)]
 struct GetItemRequest {
     item_name: String,
 }
-fn get_item_info(request: &mut Request, pool: &my::Pool) -> IronResult<Response> {
+fn get_item_info(request: &mut Request, database_manager : &DatabaseManager) -> IronResult<Response> {
 	//get request json and convert to struct
 	let mut payload = String::new();
     request.body.read_to_string(&mut payload).unwrap();
-    let request: GetItemRequest = json::decode(&payload).unwrap();
+    let request: GetItemRequest = match json::decode(&payload) {
+    	Ok(r) => r,
+    	Err(err) => return Ok(Response::with((status::BadRequest, err.to_string())))
+    };
     
-    let mut statement = pool.prepare("SELECT item_name,quantity,description from test.inventory WHERE item_name=?").unwrap();
-
-    //execute the sql query with the data from the struct
-    let selected_items: Vec<Item> = statement.execute((request.item_name,))    
-    .map(|result| {
-    	// In this closure we sill map `QueryResult` to `Vec<Payment>`
-        // `QueryResult` is iterator over `MyResult<row, err>` so first call to `map`
-        // will map each `MyResult` to contained `row` (no proper error handling)
-        // and second call to `map` will map each `row` to `Payment`
-        result.map(|x| x.unwrap()).map(|row| {
-            let (item_name, quantity, description) = my::from_row(row);
-            Item {
-                item_name: item_name,
-                quantity: quantity,
-                description: description,
-            }
-        }).collect() // Collect payments so now `QueryResult` is mapped to `Vec<Payment>`
-    }).unwrap(); // Unwrap `Vec<Payment>`
+    let selected_items = match 
+    database_manager.results_from_database(
+    	format!("SELECT {0} from test.inventory WHERE item_name={1}",MYSQL_DB_COLS, request.item_name)
+    ) 
+    {
+    	Ok(s_i) => s_i,
+    	Err(err) => return Ok(Response::with((status::BadRequest, err.to_string())))
+    };
     
     //convert the sql result to json and return
     let payload = json::encode(&selected_items).unwrap();
     Ok(Response::with((status::Ok, payload)))
 }
 
-fn add_items_to_inventory(request: &mut Request, pool: &my::Pool) -> IronResult<Response> {
+fn add_items_to_inventory(request: &mut Request, database_manager : &DatabaseManager) -> IronResult<Response> {
 	//takes json array of Items
 	let mut payload = String::new();
     request.body.read_to_string(&mut payload).unwrap();
@@ -64,18 +110,7 @@ fn add_items_to_inventory(request: &mut Request, pool: &my::Pool) -> IronResult<
     	Err(err) => return Ok(Response::with((status::BadRequest, err.to_string())))
     };
 
-	//allow execute of valid comands, but report errors
-	let mut errors: Vec<String> = Vec::new();
-
-	//insert into database
-	for mut stmt in pool.prepare(r"INSERT into test.inventory (item_name,quantity,description) VALUES (?,?,?)").into_iter() {
-        for i in items.iter() {
-            match stmt.execute((&i.item_name, i.quantity, &i.description)) {
-            	Ok(_) => continue,
-            	Err(err) => errors.push(err.to_string()),
-            }
-        }
-    }
+	let errors = database_manager.insert_into_database(items);
 
 	//take any errors, convert to json, and return
     let payload = json::encode(&errors).unwrap();
@@ -83,15 +118,46 @@ fn add_items_to_inventory(request: &mut Request, pool: &my::Pool) -> IronResult<
     Ok(Response::with((status::Ok, payload)))
 }
 
-fn main() {
-	let pool = my::Pool::new("mysql://root:test@localhost:3306").unwrap();
-	let pool_clone = pool.clone();//what/why?
+//json request format for search_for_item
+/*#[derive(RustcEncodable, RustcDecodable)]
+struct SearchItemRequest {
+    item_name: String,//find any item name that contains this
+    description: Option<String>,//or any item description that contains this
+}
+fn search_for_item(request: &mut Request) -> IronResult<Response> {
+	let mut payload = String::new();
+    request.body.read_to_string(&mut payload).unwrap();
+    let item_info: SearchItemRequest = match json::decode(&payload) {
+    	Ok(i) => i,
+    	Err(err) => return Ok(Response::with((status::BadRequest, err.to_string())))
+    };
+    
+	let mut statement = pool.prepare(format!("SELECT {0} from test.inventory WHERE item_name=?",MYSQL_DB_COLS));
+    
+    let results: Vec<Item> = statement.execute((item_info.item_name))    
+    .map(|result| {
+        result.map(|x| x.unwrap()).map(|row| {
+            let (item_name, quantity, description) = mysql::from_row(row);
+            Item {
+                item_name: item_name,
+                quantity: quantity,
+                description: description,
+            }
+        }).collect() // Collect payments so now `QueryResult` is mapped to `Vec<Payment>`
+    }).unwrap(); // Unwrap `Vec<Payment>`
+    
+    Ok(Response::with(status::Ok))
+}*/
 
+fn main() {
+	let database_manager = DatabaseManager {
+		pool: mysql::Pool::new("mysql://root:test@localhost:3306").unwrap(),
+	};
+	
 	let mut router = Router::new();
 	
-    router.post("/ItemInfo", move |r: &mut Request| get_item_info(r, &pool));
-    
-    router.post("/ItemAdd" , move |r: &mut Request| add_items_to_inventory(r, &pool_clone));
+    router.post("/ItemInfo", move |r: &mut Request| get_item_info(r, &database_manager));
+    router.post("/ItemAdd" , move |r: &mut Request| add_items_to_inventory(r, &database_manager));
         
     Iron::new(router).http("localhost:3000").unwrap();
     println!("On 3000");
