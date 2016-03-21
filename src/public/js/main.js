ca46140fd9860759504b104bc553c5cdca7a66cb
Max Wofford <max@maxwofford.com>
2016-03-20 21:28:04 -0400

    Move /client to /public

--- src/public/js/main.js
+++ src/public/js/main.js
@@ -2,14 +2,6 @@ function updateProgress(e) {
   console.log(e)
 }
 
-function transferFailed(e) {
-  console.warn(e)
-}
-
-function transferCanceled(e) {
-  console.warn(e)
-}
-
 function request(args) {
   // route, params, method, callback
   args.method = args.method || 'POST'
@@ -18,8 +10,8 @@ function request(args) {
   var url = "http://localhost:3000"  // change to location.origin in prod
   req.addEventListener("progress", updateProgress)
   req.addEventListener("load", args.callback)
-  req.addEventListener("error", transferFailed)
-  req.addEventListener("abort", transferCanceled)
+  req.addEventListener("error", args.callback)
+  req.addEventListener("abort", args.callback)
   req.open(args.method, url + args.route)
   req.send(args.params)
 }
@@ -32,21 +24,37 @@ function itemInfo(item) {
            callback: function(e) {
              // Do stuff with what it returns
              console.info(e)
+             if(e.type == "load"){
+               console.log('success')
+             } else {
+               console.log('error')
+             }
            }
-          })
+          }
+         )
 }
 
 function itemSearch(item) {
   // Expects item to be a json string
   // {"item_name_or_description":"test"}
+  document.querySelector('#search-bar').disabled = true
   request({route: '/ItemSearch',
            item: {item_name_or_description: item},
            method: "GET",
            callback: function(e) {
              // Do stuff with what it returns
-             console.info(e)
+             document.querySelector('#search-bar').disabled = false
+             if(e.type == "load"){
+               console.log('success')
+               document.querySelector('item-name').value = e.data.item_name
+               document.querySelector('description').value = e.data.description
+               document.querySelector('quantity').value = e.data.quantity
+             } else {
+               console.log('error')
+             }
            }
-          })
+          }
+         )
 }
 
 function itemAdd(item) {
@@ -55,8 +63,14 @@ function itemAdd(item) {
            item: item,
            callback: function(e) {
              console.log(e)
+             if(e.type == "load"){
+               console.log('success')
+             } else {
+               console.log('error')
+             }
            }
-          })
+          }
+         )
 }
 
 function itemUpdate(item) {
@@ -65,10 +79,25 @@ function itemUpdate(item) {
            item: item,
            callback: function(e) {
              console.log(e)
+             if(e.type == "load"){
+               console.log('success')
+             } else {
+               console.log('error')
+             }
            }
-          })
+          }
+         )
 }
 
 // function itemFind(item) {
 // TODO: This is route is subject to change, so we'll deal with it later
 // }
+
+document.querySelector('#search-bar').onkeypress = function(e) {
+  if (!e) e = window.event
+  var keyCode = e.keyCode || e.which
+  if (keyCode == '13'){
+    var query = document.querySelector('#search-bar').value
+    itemSearch(query)
+  }
+}
